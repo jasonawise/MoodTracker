a4fa9d080f39f92fe8c00ee1d30f9dc86cad8567
Jason A. Wise <jasonandrewwise@gmail.com>
2023-01-26 19:11:06 -0500

    Initial Commit

---
+++ MoodTracker/MoodTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoodTrackerApp.swift
+//  MoodTracker
+//
+//  Created by Jason Wise on 1/26/23.
+//
+
+import SwiftUI
+
+@main
+struct MoodTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
