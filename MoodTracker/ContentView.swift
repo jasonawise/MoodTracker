df096c7ec52cb8e9914dba130a13e9f8c8910024
Jason A. Wise <jasonandrewwise@gmail.com>
2023-01-26 20:00:58 -0500

    first commit

--- MoodTracker/ContentView.swift
+++ MoodTracker/ContentView.swift
@@ -8,14 +8,17 @@
 import SwiftUI
 
 struct ContentView: View {
+  
+  @State private var selectedRating = 1
+  
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+      List {
+        Picker("Mood Rating", selection: $selectedRating ) {
+          ForEach(1..<11) {
+            Text("\($0)").tag($0)
+          }
         }
-        .padding()
+      }
     }
 }
 
